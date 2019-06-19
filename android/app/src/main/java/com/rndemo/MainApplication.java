0c4112bd2cce7432092745b136aa9a64593b02e1
Carson <375162094@qq.com>
2019-06-19 16:57:15 +0800

    +    "buffer": "^5.2.1", +    "entities": "^1.1.2", +    "events": "^3.0.0", +    "htmlparser2": "^3.10.1", +    "prop-types": "^15.7.2",      "react": "16.6.3", -    "react-native": "0.58.6" +    "react-native": "0.58.6", +    "react-native-gesture-handler": "^1.3.0", +    "react-native-htmlview": "^0.14.0", +    "react-native-picker-select": "^6.2.0", +    "react-native-safe-area-view": "^0.14.5", +    "react-native-status-bar-height": "^2.3.1", +    "react-native-tableview": "^2.5.0", +    "react-native-video": "^4.4.1", +    "react-native-webview": "^5.12.0", +    "react-navigation": "^3.11.0", +    "teaset": "^0.6.3"

--- android/app/src/main/java/com/rndemo/MainApplication.java
+++ android/app/src/main/java/com/rndemo/MainApplication.java
@@ -3,6 +3,9 @@ package com.rndemo;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.reactnativecommunity.webview.RNCWebViewPackage;
+import com.brentvatne.react.ReactVideoPackage;
+import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -22,7 +25,10 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+            new RNCWebViewPackage(),
+            new ReactVideoPackage(),
+            new RNGestureHandlerPackage()
       );
     }
 
